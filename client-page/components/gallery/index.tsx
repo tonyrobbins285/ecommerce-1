42254de3e1995432283faee8539d9fa92bc868cb
tonyrobbins285 <121783750+tonyrobbins285@users.noreply.github.com>
2024-01-05 23:53:00 -0600

    In Progress: Gallery

--- client-page/components/gallery/index.tsx
+++ client-page/components/gallery/index.tsx
@@ -1,9 +1,24 @@
-import { Image } from '@/types';
+'use client';
+
+import { Image as ImageType } from '@/types';
+import { Tab } from '@headlessui/react';
+import Image from 'next/image';
+import GallaryTab from './gallery-tab';
 
 type GalleryProps = {
-  images: Image[];
+  images: ImageType[];
 };
 
 export default function Gallery({ images }: GalleryProps) {
-  return <div>Gallery</div>;
+  return (
+    <Tab.Group as="div" className="flex flex-col-reverse">
+      <div className="mx-auto mt-6 hidden w-full max-w-2xl sm:block lg:max-w-none">
+        <Tab.List className="grid grid-cols-4 gap-6">
+          {images.map((image) => (
+            <GallaryTab key={image.id} image={image} />
+          ))}
+        </Tab.List>
+      </div>
+    </Tab.Group>
+  );
 }
